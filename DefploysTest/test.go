5dd520fddfbd518161797275521764c81400a8ca
CocosGames <support@cocosgames.com>
2019-01-09 09:27:15 +0800

    init
    v 0.1

---
+++ DefploysTest/test.go
@@ -0,0 +1,298 @@
+embedded_components {
+	id: "collisionobject1"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly1.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject2"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly2.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject3"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly3.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject4"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly4.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject5"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly5.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject6"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly6.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject7"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly7.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject8"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly8.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject9"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly9.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject10"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly10.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	embedded_components {
+	id: "collisionobject11"
+	type: "collisionobject"
+	data: "collision_shape: \"/test_poly11.convexshape\"\n"
+	"type: COLLISION_OBJECT_TYPE_STATIC\n"
+	"mass: 0.0\n"
+	"friction: 0.1\n"
+	"restitution: 0.5\n"
+	"group: \"default\"\n"
+	"mask: \"default\"\n"
+	"linear_damping: 0.0\n"
+	"angular_damping: 0.0\n"
+	"locked_rotation: false\n"
+	""
+	position {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+	}
+	rotation {
+		x: 0.0
+		y: 0.0
+		z: 0.0
+		w: 1.0
+	}
+}
+	
+	
